c312a5c41911bf9ae579d91ab4bf05dccdb966db
Owlface Games <108976740+OwlfaceGames@users.noreply.github.com>
2025-05-08 19:32:15 +0100

    tweak script

--- lookup_cheat_sheet.sh
+++ lookup_cheat_sheet.sh
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env bash
 languages=`echo "golang lua cpp c typescript nodejs" | tr ' ' '\n'`
 core_utils=`echo "xargs find mv sed awk" | tr ' ' '\n'`
@@ -5,8 +6,9 @@ core_utils=`echo "xargs find mv sed awk" | tr ' ' '\n'`
 selected=`printf "$languages\n$core_utils" | fzf`
 read -p "query: " query
 
-if printf "$languages" | grep -qs $selected; then
-    tmux neww bash -c "curl cht.sh/$selected/`echo $query | tr ' ' '+'` & while [ : ]; do sleep 1; done"
+if printf "$languages\n" | grep -q "^$selected$"; then
+    tmux neww bash -c "curl -s cht.sh/$selected/`echo $query | tr ' ' '+'` | less -R"
 else
-    tmux neww bash -c "curl cht.sh/$selected~$query & while [ : ]; do sleep 1; done"
+    tmux neww bash -c "curl -s cht.sh/$selected~$query | less -R"
 fi
+
